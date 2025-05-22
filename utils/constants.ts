4634707adb753b9324c4c43de0bf4ed2ecb680d8
NamrithaGirish <namrithac1723@gmail.com>
2025-05-23 01:45:23 +0530

    jwt secret updated

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,6 +1,6 @@
-export const JWT_SECRET = "secret";//get from env
+export const JWT_SECRET = process.env.JWT_SECRET;//get from env
 
-export const JWT_VALIDITY = "1h";
+export const JWT_VALIDITY = process.env.JWT_VALIDITY;
 
 
 
